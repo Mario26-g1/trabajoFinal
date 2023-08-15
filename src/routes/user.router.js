691c5563982cb5d8a4e46d5d35fca420a33c6555
msilvasaquiray@gmail.com <msilvasaquiray@gmail.com>
2023-08-14 22:07:24 -0500

    update

---
+++ src/routes/user.router.js
@@ -0,0 +1,19 @@
+const { verifyJwt } = require('../utils/verifyJWT');
+const { getAll, create, remove, update, login } = require('../controllers/user.controllers');
+const express = require('express');
+
+const routerUser = express.Router();
+
+routerUser.route('/')
+    .get(verifyJwt, getAll)
+    .post(create);
+
+
+routerUser.route('/login')
+    .post(login)
+
+routerUser.route('/:id')
+    .delete(verifyJwt, remove)
+    .put(verifyJwt, update);
+
+module.exports = routerUser;
