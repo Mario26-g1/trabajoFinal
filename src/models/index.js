d3dec44055053ae02d9299ec9b4d3b9e8c36d1d2
msilvasaquiray@gmail.com <msilvasaquiray@gmail.com>
2023-08-17 16:39:00 -0500

    update3

--- src/models/index.js
+++ src/models/index.js
@@ -1,1 +1,12 @@
+
+const Category = require("./Category");
+const Product = require("./Product");
 const User = require("./User");
+
+
+Product.belongsTo(Category)
+Category.hasMany(Product)
+
+
+
+
