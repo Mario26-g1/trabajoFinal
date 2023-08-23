3c99f608e82aecffdf7b2ad4ebcfd1a7519262a6
msilvasaquiray@gmail.com <msilvasaquiray@gmail.com>
2023-08-23 18:53:43 -0500

    update

--- src/routes/productImg.router.js
+++ src/routes/productImg.router.js
@@ -1,4 +1,5 @@
-const { getAll, create, remove } = require('../controllers/productImg.controllers');
+const { getAll } = require('../controllers/productImg.controllers');
+const { create, remove } = require('../controllers/productImgCloudinary.controllers')
 const express = require('express');
 const upload = require('../utils/multer');
 
